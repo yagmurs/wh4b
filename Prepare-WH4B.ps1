e6856ccf0ae877c90e6cc8f6182fba05a1bfd092
GokhanOzdamar <40590389+GokhanOzdamar@users.noreply.github.com>
2018-09-08 19:58:33 +0300

    v0.1.2

--- Prepare-WH4B.ps1
+++ Prepare-WH4B.ps1
@@ -10,12 +10,12 @@
 
 
 #Create the KeyCredential Admins Security Global Group
-$KeyCredentialGroupOU = "CN=Users,DC=toynak,DC=local"
-New-ADGroup -Name "Key Credential Admins" -SamAccountName KeyCredentialAdmins -GroupCategory Security -GroupScope Global -DisplayName "Key Credential Admins" -Path $KeyCredentialGroupOU -Description "Members of this group can add and remove WH4B keys."
+$keyCredentialGroupOU = "CN=Users,DC=toynak,DC=local"
+New-ADGroup -Name "Key Credential Admins" -SamAccountName KeyCredentialAdmins -GroupCategory Security -GroupScope Global -DisplayName "Key Credential Admins" -Path $keyCredentialGroupOU -Description "Members of this group can add and remove WH4B keys."
 
 #Create the Windows Hello for Business Users Security Global Group
-$WH4BUsersGroupOU = "CN=Users,DC=toynak,DC=local"
-New-ADGroup -Name "Windows Hello for Business Users" -SamAccountName WH4BUsers -GroupCategory Security -GroupScope Global -DisplayName "Windows Hello for Business Users" -Path $WH4BUsersGroupOU -Description "Members of this group will be enabled for Windows Hello for Business"
+$wH4BUsersGroupOU = "CN=Users,DC=toynak,DC=local"
+New-ADGroup -Name "Windows Hello for Business Users" -SamAccountName WH4BUsers -GroupCategory Security -GroupScope Global -DisplayName "Windows Hello for Business Users" -Path $wH4BUsersGroupOU -Description "Members of this group will be enabled for Windows Hello for Business"
 
 #Install the Active Directory Certificate Services role
 Add-WindowsFeature Adcs-Cert-Authority –IncludeManagementTools
@@ -26,32 +26,28 @@ Install-AdcsCertificationAuthority
 #PreRequisites Powershell 5.x
 #Enterprise Admin rights
 #Download JSON
-
 Set-ExecutionPolicy Unrestricted
 Install-Module ADCSTemplate
 
+#Create Working Directory
+$deploymentSource = "C:\WH4BDeployment"
+New-Item -Path $deploymentSource -ItemType Directory -Force
+Set-Location -Path $deploymentSource
 
 #Configure Domain Controller Certificates
-Set-ExecutionPolicy Unrestricted
-Install-Module adcstemplate
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/yagmurs/wh4b/master/wh4b-adcs-dc-template.json" -OutFile $deploymentSource\wh4b-adcs-dc-template.json
+New-ADCSTemplate -DisplayName "Domain Controller Authentication (Kerberos)" -JSON (Get-Content -Path $deploymentSource\wh4b-adcs-dc-template.json -Raw) 
 
-$deploymetSource = "C:\W4HbDeployment"
-
-New-Item -Path $deploymetSource -ItemType Directory -Force
-Set-Location -Path $deploymetSource
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/yagmurs/wh4b/master/wh4b-adcs-dc-template.json" -OutFile $deploymetSource\wh4b-adcs-dc-template.json
-New-ADCSTemplate -DisplayName "Domain Controller Authentication (Kerberos)" -JSON (Get-Content -Path C:\W4HbDeployment\wh4b-adcs-dc-template.json -Raw) 
-
-#Superseding the existing Domain Controller certificate
+#Superseding the existing Domain Controller Certificate
 #Configure an Internal Web Server Certificate template
 #Unpublish Superseded Certificate Templates
 #Publish Certificate Templates to the Certificate Authority
 #Configure Domain Controllers for Automatic Certificate Enrollment
 #Deploy the Domain Controller Auto Certificate Enrollment Group Policy Object
 #Prepare and Deploy Windows Server 2016 Active Directory Federation Services
-$CertificateThumbprint = ""
-$FederationServiceName = "sts.corp.contoso.com"
-$GroupManagedServiceAccount = "CONTOSO\gmsa_ADFS"
-Install-AdfsFarm -CertificateThumbprint $CertificateThumbprint -FederationServiceName $FederationServiceName -GroupServiceAccountIdentifier $GroupManagedServiceAccount
+$certificateThumbprint = ""
+$federationServiceName = "sts.corp.contoso.com"
+$groupManagedServiceAccount = "CONTOSO\gmsa_ADFS"
+Install-AdfsFarm -CertificateThumbprint $certificateThumbprint -FederationServiceName $federationServiceName -GroupServiceAccountIdentifier $groupManagedServiceAccount
 #Validate and Deploy Multifactor Authentication Services (MFA)
 #Configure Windows Hello for Business Policy settings
